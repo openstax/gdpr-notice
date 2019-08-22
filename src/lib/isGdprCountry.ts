3a855fe4170adf099f0a3d54b87bc144840c8f26
tom <tom.g.woodward@gmail.com>
2019-08-22 10:06:33 -0400

    sort countries by name instead of code

--- src/lib/isGdprCountry.ts
+++ src/lib/isGdprCountry.ts
@@ -2,48 +2,50 @@
 // Source: http://www.ip2country.net/ip2country/country_code.html
 // Note: Azores and Madeira are Portugal, Canary Islands are Spain
 // Channel Islands, Northern Ireland, England, Wales, and Scotland constitute the United Kingdom
-const excludedCountries = {
-  AT: 'Austria',
-  BE: 'Belgium',
-  BG: 'Bulgaria',
-  CY: 'Cyprus',
-  CZ: 'Czech Republic',
-  DE: 'Germany',
-  DK: 'Denmark',
-  EE: 'Estonia',
-  ES: 'Spain',
-  FI: 'Finland',
-  FR: 'France',
-  GB: 'United Kingdom',
-  GF: 'French Guiana',
-  GP: 'Guadeloupe',
-  GR: 'Greece',
-  HR: 'Croatia',
-  HU: 'Hungary',
-  IE: 'Ireland',
-  IS: 'Iceland',
-  IT: 'Italy',
-  LI: 'Liechtenstein',
-  LT: 'Lithuania',
-  LU: 'Luxembourg',
-  LV: 'Latvia',
-  MF: 'Saint Martin',
-  MQ: 'Martinique',
-  MT: 'Malta',
-  NL: 'Netherlands',
-  NO: 'Norway',
-  PL: 'Poland',
-  PT: 'Portugal',
-  RE: 'Reunion',
-  RO: 'Romania',
-  SE: 'Sweden',
-  SI: 'Slovenia',
-  SK: 'Slovakia',
-  YT: 'Mayotte'
-};
+
+// prettier-ignore
+const excludedCountries = [
+  /* Austria */ 'AT',
+  /* Belgium */ 'BE',
+  /* Bulgaria */ 'BG',
+  /* Croatia */ 'HR',
+  /* Cyprus */ 'CY',
+  /* Czech Republic */ 'CZ',
+  /* Denmark */ 'DK',
+  /* Estonia */ 'EE',
+  /* Finland */ 'FI',
+  /* France */ 'FR',
+  /* French Guiana */ 'GF',
+  /* Germany */ 'DE',
+  /* Greece */ 'GR',
+  /* Guadeloupe */ 'GP',
+  /* Hungary */ 'HU',
+  /* Iceland */ 'IS',
+  /* Ireland */ 'IE',
+  /* Italy */ 'IT',
+  /* Latvia */ 'LV',
+  /* Liechtenstein */ 'LI',
+  /* Lithuania */ 'LT',
+  /* Luxembourg */ 'LU',
+  /* Malta */ 'MT',
+  /* Martinique */ 'MQ',
+  /* Mayotte */ 'YT',
+  /* Netherlands */ 'NL',
+  /* Norway */ 'NO',
+  /* Poland */ 'PL',
+  /* Portugal */ 'PT',
+  /* Reunion */ 'RE',
+  /* Romania */ 'RO',
+  /* Saint Martin */ 'MF',
+  /* Slovakia */ 'SK',
+  /* Slovenia */ 'SI',
+  /* Spain */ 'ES',
+  /* Sweden */ 'SE',
+  /* United Kingdom */ 'GB',
+];
 
 export default function isGdprCountry(): Promise<boolean> {
   return fetch('http://ip-api.com/json')
     .then(response => response.json())
-    .then(response => response.countryCode in excludedCountries);
+    .then(response => excludedCountries.includes(response.countryCode));
 }
